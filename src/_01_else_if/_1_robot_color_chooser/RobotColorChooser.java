7d6da2230399b1669cbd9a6bd17ebd3994a866b6
root <root@db78ad9aacf7>
2020-07-29 01:31:12 +0000

    cool

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -3,6 +3,8 @@ package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
@@ -15,10 +17,44 @@ public class RobotColorChooser {
         //6. If the user doesn't enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
+Robot rob=new Robot();
+for (int i = 0; i < 15; i++) {
+	
+	
 
+String robot= JOptionPane.showInputDialog("what color should the pen be");
+if(robot.contains("green"))
+	rob.setPenColor(Color.green);
+else if(robot.contains("red"))
+	rob.setPenColor(Color.red);
+else if(robot.contains("blue"))
+	rob.setPenColor(Color.blue);
+else if(robot.contains("orange"))
+	rob.setPenColor(Color.orange);
+else if(robot.contains("black"))
+	rob.setPenColor(Color.black);
+else if(robot.contains("yellow"))
+	rob.setPenColor(Color.yellow);
+else if(robot.contains("pink"))
+	rob.setPenColor(Color.pink);
+else rob.setRandomPenColor();
+
+rob.setSpeed(100);
+rob.penDown();
+rob.move(150);
+rob.turn(90);
+rob.move(150);
+rob.turn(90);
+rob.move(150);
+rob.turn(90);
+rob.move(150);
+rob.sparkle();		
 		
-
-
-
+		
+		
+}
+		
+		
+		
 	}
 }
