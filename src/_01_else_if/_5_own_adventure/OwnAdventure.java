53cf0e0919d01b0b6fc786523bd7256d195b9f5d
root <root@a2e6c45a8949>
2020-09-23 01:28:09 +0000

    nice

--- src/_01_else_if/_5_own_adventure/OwnAdventure.java
+++ src/_01_else_if/_5_own_adventure/OwnAdventure.java
@@ -1,7 +1,9 @@
 package _01_else_if._5_own_adventure;
 
 public class OwnAdventure {
-
+public static void main(String[] args) {
+	
+}
 	
 }
 
---
+++ src/_02_loop_variables/_1_years_alive/Yearsalive.java
@@ -0,0 +1,9 @@
+package _02_loop_variables._1_years_alive;
+
+public class Yearsalive {
+public static void main(String[] args) {
+	for (int i =2006 ; i < 2021; i++) {
+	System.out.println(i);	
+	}
+}
+}
